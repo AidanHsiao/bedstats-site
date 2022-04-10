1b26fca1f5b8d415a0bf3cee671fe8605f27a61d
AidanHsiao <49498006+AidanHsiao@users.noreply.github.com>
2022-04-10 13:06:12 -0700

    Rewriting database call code

--- src/pages/api/users/recordstats.ts
+++ src/pages/api/users/recordstats.ts
@@ -3,36 +3,50 @@ import getUserList from "../../../../lib/db/getUserList";
 import getFirestore from "../../../../lib/db/initializeDB";
 import getStats from "../../../../lib/getStats";
 
+async function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
   const key = req.query.key;
   if (key === process.env.SITE_API_KEY) {
-    const data = await getUserList();
-    const keys = data.map((doc: any) => doc.hypixelAPIKey);
-    const uuids = data.map((doc: any) => doc.uuid);
+    const userList = await getUserList();
+    const keys = userList.map((doc: any) => doc.hypixelAPIKey);
+    const uuids = userList.map((doc: any) => doc.uuid);
     if (!uuids) {
       res.status(404).json({ condition: "error" });
       return;
     }
-    uuids.forEach(async (uuid: string, idx: number) => {
-      const data = await getStats(keys[idx], uuid, true);
+    let error = false;
+    let idx = 0;
+    const interval = setInterval(async () => {
+      if (idx >= keys.length) {
+        clearInterval(interval);
+        return;
+      }
+      const data = await getStats(keys[idx], uuids[idx], true);
       if (data.stats) {
         const timestamp = Math.floor(Date.now() / 1000);
         data.stats.timestamp = timestamp;
         const db = getFirestore();
-        const result = await db
+        await db
           .collection("users")
-          .doc(uuid)
+          .doc(uuids[idx])
           .collection("stats")
           .doc(`${timestamp}`)
           .set(data.stats);
       } else {
+        console.log(data);
         res.status(500).json({ code: data.code });
+        error = true;
       }
-    });
-    res.status(200).json({ condition: "success" });
+    }, 1000);
+    if (!error) {
+      res.status(200).json({ condition: "success" });
+    }
     return;
   }
   res.status(401).json({ condition: "error" });
