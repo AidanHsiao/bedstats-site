c803107b15962cd3b7a1e656ed4b4711c2b668cd
AidanHsiao <49498006+AidanHsiao@users.noreply.github.com>
2022-05-11 12:28:17 -0700

    Recreating CSP directives

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,11 +5,13 @@ import { NextStrictCSP } from "next-strict-csp";
 const HeadCSP = process.env.NODE_ENV === "production" ? NextStrictCSP : Head;
 
 export default function Document() {
+  const csp = `default-src: 'self'; script-src: 'self' ; frame-ancestors: 'self' https://bedstats-site.vercel.app`;
+
   return (
     <Html lang="en">
       <HeadCSP>
         {process.env.NODE_ENV === "production" && (
-          <meta httpEquiv="Content-Security-Policy" />
+          <meta httpEquiv="Content-Security-Policy" content={csp} />
         )}
       </HeadCSP>
       <body>
