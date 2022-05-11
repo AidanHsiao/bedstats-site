9d130016d5f054243eea0023a893c0c3104b82bd
AidanHsiao <49498006+AidanHsiao@users.noreply.github.com>
2022-05-11 11:43:16 -0700

    Last minute bug fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,10 @@
 import { AppProps } from "next/app";
 import React, { ReactElement } from "react";
 import "../common/globals.css";
-import Head from "next/head";
+import { withSecureHeaders } from "next-secure-headers";
 
-export default function BedStats({
-  Component,
-  pageProps,
-}: AppProps): ReactElement {
+function BedStats({ Component, pageProps }: AppProps): ReactElement {
   return <Component {...pageProps} />;
 }
+
+export default withSecureHeaders()(BedStats);
