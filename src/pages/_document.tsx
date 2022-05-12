bdf55bc907caa3cc7a94983435f8776a5912a3b3
AidanHsiao <49498006+AidanHsiao@users.noreply.github.com>
2022-05-12 11:27:25 -0700

    Temporarily removing CSP from _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,8 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-import { NextStrictCSP } from "next-strict-csp";
-
-const HeadCSP = process.env.NODE_ENV === "production" ? NextStrictCSP : Head;
-
 export default function Document() {
   return (
     <Html lang="en">
-      <HeadCSP>
-        {process.env.NODE_ENV === "production" && (
-          <meta
-            httpEquiv="Content-Security-Policy"
-            content="script-src 'self'; frame-ancestors 'none'"
-          />
-        )}
-      </HeadCSP>
       <body>
         <Main />
         <NextScript />
