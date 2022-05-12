cd96209c0865002b5aafae6b6bb527eb6c948349
AidanHsiao <49498006+AidanHsiao@users.noreply.github.com>
2022-05-12 11:34:15 -0700

    Testing nonce

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,34 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-import { NextStrictCSP } from "next-strict-csp";
+import crypto from "crypto";
+import { v4 } from "uuid";
 
-const HeadCSP = process.env.NODE_ENV === "production" ? NextStrictCSP : Head;
+function generateCSP() {
+  const production = process.env.NODE_ENV === "production";
+
+  const nonce = crypto.createHash("sha256").update(v4()).digest("base64");
+
+  let csp = `default-src 'none';base-uri 'self';style-src 'unsafe-inline';`;
+  csp += `script-src 'nonce-${nonce}' 'self' ${
+    production ? "" : "'unsafe-eval'"
+  };`;
+  if (!production) csp += `connect-src 'self';`;
+
+  return [csp, nonce];
+}
 
 export default function Document() {
+  const [csp, nonce] = generateCSP();
+
   return (
     <Html lang="en">
-      <Head />
+      <Head nonce={nonce}>
+        <meta property="csp-nonce" content={nonce} />
+        <meta httpEquiv="Content-Security-Policy" content={csp} />
+      </Head>
       <body>
         <Main />
-        <NextScript />
+        <NextScript nonce={nonce} />
       </body>
     </Html>
   );
