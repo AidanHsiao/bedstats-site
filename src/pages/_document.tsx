bcc5f537dc24376166735bd1254445df7236b725
AidanHsiao <49498006+AidanHsiao@users.noreply.github.com>
2022-05-12 11:29:00 -0700

    Re-adding head to document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+import { NextStrictCSP } from "next-strict-csp";
+
+const HeadCSP = process.env.NODE_ENV === "production" ? NextStrictCSP : Head;
+
 export default function Document() {
   return (
     <Html lang="en">
+      <Head />
       <body>
         <Main />
         <NextScript />
