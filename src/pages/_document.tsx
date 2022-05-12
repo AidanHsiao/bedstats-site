906f0a1259393a604895a43a46a717515e599b28
AidanHsiao <49498006+AidanHsiao@users.noreply.github.com>
2022-05-12 11:51:31 -0700

    Allowing font and images to load

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,7 @@ function generateCSP() {
 
   const whitelistedUrls = ["https://www.gstatic.com"];
 
-  let csp = `default-src 'none'; base-uri 'self'; style-src 'unsafe-inline' 'self' ${whitelistedUrls.join(
+  let csp = `default-src 'none'; base-uri 'self'; font-src 'self'; img-src 'self'; style-src 'unsafe-inline' 'self' ${whitelistedUrls.join(
     " "
   )};`;
   csp += `script-src ${whitelistedUrls.join(" ")} 'nonce-${nonce}' 'self' ${
